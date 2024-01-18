151940b71d956e55dfe36babc93ca20cf4c664fe
Zin Ko Ko <zinkoko.dev@gmail.com>
2024-01-19 01:15:48 +0630

    Add ChatHub.cs, Startup.cs and Index.html

---
+++ SignalR.Chat.DotNetFrameWork/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+[assembly: OwinStartup(typeof(SignalR.Chat.DotNetFrameWork.Startup))]
+namespace SignalR.Chat.DotNetFrameWork
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
